3814247c653e05c546f5f52cdd66d1738a58b2d8
Maxim <maksfomin@gmail.com>
2020-01-12 02:28:53 +0300

    Create Notify push 1.2

--- app/src/main/java/ru/anatomica/messenger/MainActivity.java
+++ app/src/main/java/ru/anatomica/messenger/MainActivity.java
@@ -109,40 +109,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         refreshToken = FirebaseInstanceId.getInstance().getToken();
+
 //        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(this, instanceIdResult -> {
 //            refreshToken = instanceIdResult.getToken();
 //            Log.e("refreshToken", refreshToken);
 //        });
+//        intent = new Intent(this, MainActivity.class);
 
-        Button logTokenButton = findViewById(R.id.logTokenButton);
-        logTokenButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Get token
-                // [START retrieve_current_token]
-                FirebaseInstanceId.getInstance().getInstanceId()
-                        .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<InstanceIdResult> task) {
-                                if (!task.isSuccessful()) {
-                                    Log.w(TAG, "getInstanceId failed", task.getException());
-                                    return;
-                                }
-
-                                // Get new Instance ID token
-                                String token = task.getResult().getToken();
-
-                                // Log and toast
-                                String msg = getString(R.string.msg_token_fmt, token);
-                                Log.d(TAG, msg);
-                                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                // [END retrieve_current_token]
-            }
-        });
-
-        // intent = new Intent(this, MainActivity.class);
         initialize();
     }
 
